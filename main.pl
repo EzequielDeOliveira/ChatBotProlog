7538cf2915856468838d172324219d70bceb32df
Guilherme-Aguiar <gui9627oli@gmail.com>
2019-10-12 16:20:20 -0300

    Refactoring rules

--- main.pl
+++ main.pl
@@ -80,9 +80,9 @@ pattern([X,is,1,pokemon,'?'],[yes,X,is,a,'pokemon -',Y]) :- pokemon(X,Y).
 
 %% Padrões do tipo do pokemon
 % Quando o pokemon tem 1 tipo
-pattern([what,X,type],[X,type,is,Elem]) :- bagof(Y,type(X,Y),Bag),length(Bag, 1),nth0(0,Bag,Elem).
+pattern([what,X,type],[X,type,is,Elem]) :- getPokemonType(X,Elem).
 %Quando o pokemon tem 2 tipos
-pattern([what,X,type],[X,type,is,Elem,and,Elem2]) :- bagof(Y,type(X,Y),Bag),nth0(0,Bag,Elem),nth0(1,Bag,Elem2).
+pattern([what,X,type],[X,type,is,Elem,and,Elem2]) :- getPokemonType(X,Elem,Elem2).
 
 %% Padrões da altura do pokemon
 pattern([what,1,X,height,2],[X,height,is,Y]) :- height(X,Y).
@@ -92,46 +92,27 @@ pattern([what,1,X,weight,2],[X,weight,is,Y]) :- weight(X,Y).
 pattern([how,much,1,X,weights,2],[X,weight,is,Y]) :- weight(X,Y).
 
 %%Padrões de evolução do pokemon
-pattern([what,1,X,evolution],[X,evolution,is,Elem]) :- bagof(Y,next_evolution(X,Y),Bag),length(Bag, 1),nth0(0,Bag,Elem).
-pattern([what,1,X,evolutions],[X,evolution,is,Elem,and,Elem2]) :- bagof(Y,next_evolution(X,Y),Bag),length(Bag, 2),nth0(0,Bag,Elem),nth0(1,Bag,Elem2).
+pattern([what,1,X,evolution],[X,evolution,is,Elem]) :- getPokemonEvolution(X,Elem).
+pattern([what,1,X,evolutions],[X,evolution,are,Elem,and,Elem2]) :- getPokemonEvolution(X,Elem,Elem2).
 
 %Padrões de desevolução do pokemon
-pattern([what,1,X,previous,evolution],[X,previous,evolution,is,Elem]) :- bagof(Y,prev_evolution(X,Y),Bag),length(Bag, 1),nth0(0,Bag,Elem).
-pattern([what,1,X,previous,evolutions],[X,previous,evolution,is,Elem,and,Elem2]) :- 
-    bagof(Y,prev_evolution(X,Y),Bag),length(Bag, 2),nth0(0,Bag,Elem),nth0(1,Bag,Elem2).
+pattern([what,1,X,previous,evolution],[X,previous,evolution,is,Elem]) :- getPokemonPreviousEvolution(X,Elem).
+pattern([what,1,X,previous,evolutions],[X,previous,evolution,are,Elem,and,Elem2]) :- getPokemonPreviousEvolution(X,Elem,Elem2).
 
 
 %Padrões de comparação de peso de 2 pokemons 
-pattern([is,X,heavier,than,Y],[yes,X,is,heavier,than,Y]) :- 
-    weight(X,PX), weight(Y,PY), 
-    atom_number(PX,PXINT), atom_number(PY,PYINT),
-    PXINT > PYINT.
+pattern([is,X,heavier,than,Y],[yes,X,is,heavier,than,Y]) :- isPokemonHeavier(X,Y).
 
-pattern([is,X,heavier,than,Y],[no,X,is,lighter,than,Y]) :- 
-    weight(X,PX), weight(Y,PY), 
-    atom_number(PX,PXINT), atom_number(PY,PYINT),
-    PXINT < PYINT.
+pattern([is,X,heavier,than,Y],[no,X,is,lighter,than,Y]) :- isPokemonNotHeavier(X,Y).
 
-pattern([is,X,heavier,than,Y],[X,'weight is equal to',Y]) :- 
-    weight(X,PX), weight(Y,PY), 
-    atom_number(PX,PXINT), atom_number(PY,PYINT),
-    PXINT = PYINT.
+pattern([is,X,heavier,than,Y],[X,'weight is equal to',Y]) :- isPokemonWeightEqual(X,Y).
 
 %Padrões de comparação de altura de 2 pokemons 
-pattern([is,X,higher,than,Y],[yes,X,is,higher,than,Y]) :- 
-    height(X,PX), height(Y,PY), 
-    atom_number(PX,PXINT), atom_number(PY,PYINT),
-    PXINT > PYINT.
+pattern([is,X,higher,than,Y],[yes,X,is,higher,than,Y]) :- isPokemonHigher(X,Y).
 
-pattern([is,X,higher,than,Y],[no,X,is,lower,than,Y]) :- 
-    height(X,PX), height(Y,PY), 
-    atom_number(PX,PXINT), atom_number(PY,PYINT),
-    PXINT < PYINT.
+pattern([is,X,higher,than,Y],[no,X,is,lower,than,Y]) :- isPokemonNotHigher(X,Y).
 
-pattern([is,X,higher,than,Y],[X,'height is equal to',Y,height]) :- 
-    height(X,PX), height(Y,PY), 
-    atom_number(PX,PXINT), atom_number(PY,PYINT),
-    PXINT = PYINT.
+pattern([is,X,higher,than,Y],[X,'height is equal to',Y,height]) :- isPokemonHeightEqual(X,Y).
 
 %Padrões de vantagem de pokemon
 pattern([1,2,X,has,advantage,over,T],[yes, X,'has advantage over',T]) :- advantage(X,T).
@@ -143,10 +124,6 @@ pattern([1,2,X,has,advantage,over,T],[no, X,'has disadvantage over',T]) :- disad
 pattern([1,type,X,has,advantage,over,Y],[yes, X,'has advantage over',Y]) :- strong(X,Y).
 
 pattern([1,type,X,has,advantage,over,Y],[no, X,'has disadvantage over',Y]) :- weak(X,Y).
-
-
-
-
 
 pattern([1],[please,go,on]).
 
--- pokerules.pl
+++ pokerules.pl
@@ -6,3 +6,27 @@ advantage(X,Y):-pokemon(X,_),type(X,Z),strong(Z,Y).
 disadvantage(X,Y):-pokemon(X,_),type(X,Z),weak(Z,Y).
 
 hasadvantage(X,Y):-type(X,Z),type(Y,W),strong(Z,W).
+
+getPokemonType(X,Elem) :- bagof(Y,type(X,Y),Bag),length(Bag, 1),nth0(0,Bag,Elem).
+
+getPokemonType(X,Elem,Elem2) :- bagof(Y,type(X,Y),Bag),nth0(0,Bag,Elem),nth0(1,Bag,Elem2).
+
+getPokemonEvolution(X,Elem) :- bagof(Y,next_evolution(X,Y),Bag),length(Bag, 1),nth0(0,Bag,Elem).
+
+getPokemonEvolution(X,Elem,Elem2) :- bagof(Y,next_evolution(X,Y),Bag),length(Bag, 2),nth0(0,Bag,Elem),nth0(1,Bag,Elem2).
+
+getPokemonPreviousEvolution(X,Elem) :- bagof(Y,prev_evolution(X,Y),Bag),length(Bag, 1),nth0(0,Bag,Elem).
+
+getPokemonPreviousEvolution(X,Elem,Elem2) :- bagof(Y,prev_evolution(X,Y),Bag),length(Bag, 2),nth0(0,Bag,Elem),nth0(1,Bag,Elem2).
+
+isPokemonHeavier(X,Y) :- weight(X,PX), weight(Y,PY), atom_number(PX,PXINT), atom_number(PY,PYINT), PXINT > PYINT.
+
+isPokemonNotHeavier(X,Y) :- weight(X,PX), weight(Y,PY), atom_number(PX,PXINT), atom_number(PY,PYINT), PXINT < PYINT.
+
+isPokemonWeightEqual(X,Y) :- weight(X,PX), weight(Y,PY), atom_number(PX,PXINT), atom_number(PY,PYINT), PXINT = PYINT.
+
+isPokemonHigher(X,Y) :- height(X,PX), height(Y,PY), atom_number(PX,PXINT), atom_number(PY,PYINT),PXINT > PYINT.
+
+isPokemonNotHigher(X,Y) :- height(X,PX), height(Y,PY), atom_number(PX,PXINT), atom_number(PY,PYINT),PXINT < PYINT.
+
+isPokemonHeightEqual(X,Y) :- height(X,PX), height(Y,PY), atom_number(PX,PXINT), atom_number(PY,PYINT),PXINT = PYINT.
