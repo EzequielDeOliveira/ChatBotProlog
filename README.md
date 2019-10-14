411f25680627c9664c568b5698b2cb5fb3704c10
Gabriel Carvalho <gabriel315df@gmail.com>
2019-10-14 01:08:35 -0300

    Added readme and fixed patterns

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
-# Prolog
+# O que é
+Pokelog é um chatbot que responde perguntas sobre pokemon, o tema das perguntas são o nome, número, tipo, peso, altura, proxima evolução, evolução anterior, vantagens do pokemon, desvantagens e imunidades. O programa busca simular uma conversa. O usuário escreve uma sentença e o bot dá uma resposta apropriada. O pokelog reconhece padrões entre as palavras e responde de acordo com a padrão de resposta correspondente.
+O Pokelog é baseado na ELIZA, um clássico da inteligência artificial e o primeiro software para simulação de diálogos. Ela funciona com base no principio do procedimento é o match da entrada e um padrão. O resultado desse match é aplicado à outro padrão para determinar a resposta. O par de padrões pode ser considerado um par de estímulo/resposta, onde a entrada é comparada(matched) com o estímulo e a saída gerada da resposta. Um típico par estímulo/resposta é:
+> - `Qual é a altura do {pokemon}     A altura do {pokemon} é X metros `
+
+Usando esse par, a resposta do programa para a entrada  `Qual é a altura do pikachu`, vai ser  `A altura do {pokemon} é X metros `. O  `{pokemon}` pode ser visto como um slot a ser preenchido. O pokelog, sendo versão simplificada da ELIZA, implementa o seguinte algoritmo:
+1. Lê o input
+2. Enquanto o input não é  **`bye`**:
+3. Escolhe um par estímulo/resposta,
+4. Faz o match do input com o estímulo,
+5. Gera a o output da resposta e o match acima,
+6. Exibe a resposta,
+7. Lê o próximo input.
+
+
+# Sobre a base de conhecimento do Pokelog:
+
+Os fatos disponíveis no arquivo [pokelist.py](pokelist.py) são:
+> - `pokemon(?Nome:string, ?Número:string)`
+> - `type(?Nome:string, ?Número:string)`
+> - `height(?Nome:string, ?Altura:string)`
+> - `weight(?Nome:string, ?Peso:string)`
+> - `next_evolution(?Nome:string, ?NomeProximaEvolucao:string)`
+> - `prev_evolution(?Nome:string, ?NomeEvolucaoAnterior:string)`
+
+Os fatos disponíveis no arquivo [poketypes.py](poketypes.py) são:
+> - `immune(?Tipo:string, ?Tipo:string)`
+> - `weak(?Tipo:string, ?Tipo:string)`
+> - `strong(?Tipo:string, ?Tipo:string)`
+
+-------------
+
+## Instalação e Execução
+
+1. Instale o SWI-Prolog.
+2. No diretório do projeto, execute **`swipl`** no terminal.
+3. Execute **`[main].`** no terminal.
+4. Execute **`pokelog.`** no terminal.
+5. Converse com o pokelog!
+
+
+# Perguntas que serão respondidas
+As perguntas listadas a seguir contém apenas sentenças que o bot entenderá 
+
+- who is **`nome do pokemon`**?
+- **`nome do pokemon`**
+- tell me about **`nome do pokemon`**
+- is **`nome do pokemon`** a pokemon?
+- what type is **`nome do pokemon`**?
+- what is **`nome do pokemon`** height?
+- what is **`nome do pokemon`** previous evolution?
+- what is **`nome do pokemon`** previous evolutions?
+- what is **`nome do pokemon`** evolution?
+- what is **`nome do pokemon`** evolutions?
+- is **`nome do pokemon`** havier than **`nome do pokemon`**?
+- is **`nome do pokemon`** higher than **`nome do pokemon`**?
+- **`nome do pokemon`** has advantage over **`nome do pokemon`**?
+- type **`tipo do pokemon`** has advantage over **`tipo do pokemon`**?
+- what are **`nome do pokemon`** weaknesses?
+- what is  **`nome do pokemon`** strong against?
+- what is  **`nome do pokemon`** imunne against?
+
+-------
+
+# Refêrencias 
+
+STERLING,  Leon. [The Art Of Prolog](https://www.amazon.com/Art-Prolog-Second-Programming-Techniques/dp/0262193388
+): 2ª Edição. Cambridge: The MIT Press, 1999.
+
+
--- patterns.pl
+++ patterns.pl
@@ -156,15 +156,15 @@ pattern([who, is, X], [X, -, N, '\n', 'type: ', Y, '\n', 'height: ', H, '\n', we
 
 
 % Padrões do nome e número do pokemon
-pattern([X, is, 1, pokemon, ?], [yes, X, is, a, 'pokemon -', Y]) :-
+pattern([is, X,  1, pokemon, ?], [yes, X, is, a, 'pokemon -', Y]) :-
     pokemon(X, Y).
 
 %% Padrões do tipo do pokemon
 % Quando o pokemon tem 1 tipo
-pattern([what, X, type], [X, type, is, Elem]) :-
+pattern([what, type, is, X,1], [X, type, is, Elem]) :-
     getPokemonType(X, Elem).
 %Quando o pokemon tem 2 tipos
-pattern([what, X, type], [X, type, is, Elem, and, Elem2]) :-
+pattern([what, type, is, X,1], [X, type, is, Elem, and, Elem2]) :-
     getPokemonType(X, Elem, Elem2).
 
 %% Padrões da altura do pokemon
@@ -178,67 +178,67 @@ pattern([how, much, 1, X, weights, 2], [X, weight, is, Y]) :-
     weight(X, Y).
 
 %%Padrões de evolução do pokemon
-pattern([what, 1, X, evolution], [X, evolution, is, Elem]) :-
+pattern([what, 1, X, evolution,2], [X, evolution, is, Elem]) :-
     getPokemonEvolution(X, Elem).
-pattern([what, 1, X, evolutions], [X, evolution, are, Elem, and, Elem2]) :-
+pattern([what, 1, X, evolutions,2], [X, evolution, are, Elem, and, Elem2]) :-
     getPokemonEvolution(X, Elem, Elem2).
 
 %Padrões de desevolução do pokemon
-pattern([what, 1, X, previous, evolution], [X, previous, evolution, is, Elem]) :-
+pattern([what, 1, X, previous, evolution,2], [X, previous, evolution, is, Elem]) :-
     getPokemonPreviousEvolution(X, Elem).
-pattern([what, 1, X, previous, evolutions], [X, previous, evolution, are, Elem, and, Elem2]) :-
+pattern([what, 1, X, previous, evolutions,2], [X, previous, evolution, are, Elem, and, Elem2]) :-
     getPokemonPreviousEvolution(X, Elem, Elem2).
 
 %Padrões de comparação de peso de 2 pokemons 
-pattern([is, X, heavier, than, Y], [yes, X, is, heavier, than, Y]) :-
+pattern([is, X, heavier, than, Y,1], [yes, X, is, heavier, than, Y]) :-
     isPokemonHeavier(X, Y).
 
-pattern([is, X, heavier, than, Y], [no, X, is, lighter, than, Y]) :-
+pattern([is, X, heavier, than, Y,1], [no, X, is, lighter, than, Y]) :-
     isPokemonNotHeavier(X, Y).
 
-pattern([is, X, heavier, than, Y], [X, 'weight is equal to', Y]) :-
+pattern([is, X, heavier, than, Y,1], [X, 'weight is equal to', Y]) :-
     isPokemonWeightEqual(X, Y).
 
 %Padrões de comparação de altura de 2 pokemons 
-pattern([is, X, higher, than, Y], [yes, X, is, higher, than, Y]) :-
+pattern([is, X, higher, than, Y,1], [yes, X, is, higher, than, Y]) :-
     isPokemonHigher(X, Y).
 
-pattern([is, X, higher, than, Y], [no, X, is, lower, than, Y]) :-
+pattern([is, X, higher, than, Y,1], [no, X, is, lower, than, Y]) :-
     isPokemonNotHigher(X, Y).
 
-pattern([is, X, higher, than, Y], [X, 'height is equal to', Y, height]) :-
+pattern([is, X, higher, than, Y,1], [X, 'height is equal to', Y, height]) :-
     isPokemonHeightEqual(X, Y).
 
 %Padrões de vantagem de pokemon com um tipo
-pattern([1, 2, X, has, advantage, over, T], [yes, X, 'has advantage over', T]) :-
+pattern([1, 2, X, has, advantage, over, T,3], [yes, X, 'has advantage over', T]) :-
     advantage(X, T).
 
-pattern([1, 2, X, has, advantage, over, T], [no, X, 'has disadvantage over', T]) :-
+pattern([1, 2, X, has, advantage, over, T,3], [no, X, 'has disadvantage over', T]) :-
     disadvantage(X, T).
 
 
 %Padrões de vantagem entre tipos
-pattern([1, type, X, has, advantage, over, Y], [yes, X, 'has advantage over', Y]) :-
+pattern([1, type, X, has, advantage, over, Y,2], [yes, X, 'has advantage over', Y]) :-
     strong(X, Y).
 
-pattern([1, type, X, has, advantage, over, Y], [no, X, 'has disadvantage over', Y]) :-
+pattern([1, type, X, has, advantage, over, Y,2], [no, X, 'has disadvantage over', Y]) :-
     weak(X, Y).
 
 
 %Padrões de listagem de fraquezas de um pokemon
-pattern([what, are, X, weaknesses], FinalBag) :-
+pattern([what, are, X, weaknesses,1], FinalBag) :-
     getPokemonMultipleWeakness(X, FinalBag).
 
-pattern([what, are, X, weaknesses], [X, weakness, are, Elem1, and, Elem2]) :-
+pattern([what, are, X, weaknesses,1], [X, weakness, are, Elem1, and, Elem2]) :-
     getPokemonWeakness(X, Elem1, Elem2).
 
-pattern([what, are, X, weaknesses], [X, weakness, are, Elem1, ',', Elem2, and, Elem3]) :-
+pattern([what, are, X, weaknesses,1], [X, weakness, are, Elem1, ',', Elem2, and, Elem3]) :-
     getPokemonWeakness(X, Elem1, Elem2, Elem3).
 
-pattern([what, are, X, weaknesses], [X, weakness, are, Elem1, ',', Elem2, ',', Elem3, and, Elem4]) :-
+pattern([what, are, X, weaknesses,1], [X, weakness, are, Elem1, ',', Elem2, ',', Elem3, and, Elem4]) :-
     getPokemonWeakness(X, Elem1, Elem2, Elem3, Elem4).
 
-pattern([what, are, X, weaknesses], [X, weakness, are, Elem1, ',', Elem2, ',', Elem3, ',', Elem4, and, Elem5]) :-
+pattern([what, are, X, weaknesses,1], [X, weakness, are, Elem1, ',', Elem2, ',', Elem3, ',', Elem4, and, Elem5]) :-
     getPokemonWeakness(X,
                        Elem1,
                        Elem2,
@@ -247,19 +247,19 @@ pattern([what, are, X, weaknesses], [X, weakness, are, Elem1, ',', Elem2, ',', E
                        Elem5).
 
 %Padrões de listagem de vantagens de um pokemon
-pattern([what, is, X, strong, against], FinalBag) :-
+pattern([what, is, X, strong, against,1], FinalBag) :-
     getPokemonMultipleStrongness(X, FinalBag).
 
-pattern([what, is, X, strong, against], [X, 'is strong against', Elem1, and, Elem2]) :-
+pattern([what, is, X, strong, against,1], [X, 'is strong against', Elem1, and, Elem2]) :-
     getPokemonStrongness(X, Elem1, Elem2).
 
-pattern([what, is, X, strong, against], [X, 'is strong against', Elem1, ',', Elem2, and, Elem3]) :-
+pattern([what, is, X, strong, against,1], [X, 'is strong against', Elem1, ',', Elem2, and, Elem3]) :-
     getPokemonStrongness(X, Elem1, Elem2, Elem3).
 
-pattern([what, is, X, strong, against], [X, 'is strong against', Elem1, ',', Elem2, ',', Elem3, and, Elem4]) :-
+pattern([what, is, X, strong, against,1], [X, 'is strong against', Elem1, ',', Elem2, ',', Elem3, and, Elem4]) :-
     getPokemonStrongness(X, Elem1, Elem2, Elem3, Elem4).
 
-pattern([what, is, X, strong, against], [X, 'is strong against', Elem1, ',', Elem2, ',', Elem3, ',', Elem4, and, Elem5]) :-
+pattern([what, is, X, strong, against,1], [X, 'is strong against', Elem1, ',', Elem2, ',', Elem3, ',', Elem4, and, Elem5]) :-
     getPokemonStrongness(X,
                          Elem1,
                          Elem2,
@@ -268,13 +268,13 @@ pattern([what, is, X, strong, against], [X, 'is strong against', Elem1, ',', Ele
                          Elem5).
 
 %Padroes de listagem de imunidades de um pokemon
-pattern([what, is, X, imunne, against], FinalBag) :-
+pattern([what, is, X, imunne, against,1], FinalBag) :-
     getPokemonMultipleImunnities(X, FinalBag).
 
-pattern([what, is, X, imunne, against], [X, 'is imunne against', Elem1]) :-
+pattern([what, is, X, imunne, against,1], [X, 'is imunne against', Elem1]) :-
     getPokemonImunities(X, Elem1).
 
-pattern([what, is, X, imunne, against], [X, 'is imunne against', Elem1, and, Elem2]) :-
+pattern([what, is, X, imunne, against,1], [X, 'is imunne against', Elem1, and, Elem2]) :-
     getPokemonImunities(X, Elem1, Elem2).
 
 
